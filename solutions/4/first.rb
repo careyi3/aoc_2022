b3993e343f2af78e00a3c88efe683c56eca6840a
careyi3 <ian@iancarey.ie>
2022-12-04 09:47:27 +0000

    Day 4

--- solutions/4/first.rb
+++ solutions/4/first.rb
@@ -2,6 +2,16 @@
 
 module Day4
   module Part1
-    def self.run(path) end
+    def self.run(path)
+      count = 0
+      FileReader.for_each_line(path) do |line|
+        input = line.split(',').map { |x| x.split('-') }
+        section1 = *(input[0][0]..input[0][1])
+        section2 = *(input[1][0]..input[1][1])
+        intersection = section1.intersection(section2)
+        count += 1 if intersection == section1 || intersection == section2
+      end
+      puts count
+    end
   end
 end
---
+++ solutions/5/second.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Day5
+  module Part2
+    def self.run(path) end
+  end
+end
