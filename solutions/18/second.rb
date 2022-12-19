99c456252067e85750abdf06cd0f1148727249d8
careyi3 <ian@iancarey.ie>
2022-12-19 15:56:03 +0000

    Day 18

--- solutions/18/second.rb
+++ solutions/18/second.rb
@@ -3,25 +3,64 @@
 module Day18
   module Part2
     def self.run(path, _)
-      cubes = []
+      cubes = {}
       FileReader.for_each_line(path) do |line|
         x, y, z = line.split(',').map(&:to_i)
-        cubes << { x:, y:, z:, exposed: 6 }
+        cubes["#{x}:#{y}:#{z}"] = { x:, y:, z:, exposed: 0, exterior: 0 }
       end
-      cubes.each_with_index do |current, cidx|
-        non_unique_faces = []
-        cubes.each do |target, tidx|
-          next if cidx == tidx
 
-          non_unique_faces << [target[:x], target[:y], target[:z]] if (current[:x] == target[:x] && current[:y] == target[:y]) && (current[:z] == target[:z] + 1 || current[:z] == target[:z] - 1)
-
-          non_unique_faces << [target[:x], target[:y], target[:z]] if (current[:x] == target[:x] && current[:z] == target[:z]) && (current[:y] == target[:y] + 1 || current[:y] == target[:y] - 1)
-
-          non_unique_faces << [target[:x], target[:y], target[:z]] if (current[:z] == target[:z] && current[:y] == target[:y]) && (current[:x] == target[:x] + 1 || current[:x] == target[:x] - 1)
+      cubes.each do |_, current|
+        x = current[:x]
+        y = current[:y]
+        z = current[:z]
+        faces = {
+          [x, y, z + 1].join(':') => { coords: [x, y, z + 1] },
+          [x, y, z - 1].join(':') => { coords: [x, y, z - 1] },
+          [x, y + 1, z].join(':') => { coords: [x, y + 1, z] },
+          [x, y - 1, z].join(':') => { coords: [x, y - 1, z] },
+          [x + 1, y, z].join(':') => { coords: [x + 1, y, z] },
+          [x - 1, y, z].join(':') => { coords: [x - 1, y, z] }
+        }
+        exposed = []
+        faces.each do |key, _|
+          exposed << key if cubes[key].nil?
         end
-        current[:exposed] = current[:exposed] - non_unique_faces.uniq.count
+        current[:exposed] = exposed.size
+
+        exterior = []
+        exposed.each do |key|
+          x, y, z = faces[key][:coords]
+          exterior << key if walk(cubes, {}, x, y, z, 0)
+        end
+        current[:exterior] = exterior.size
       end
-      puts cubes.map { |x| x[:exposed] }.sum
+      puts cubes.values.map { |x| x[:exposed] }.sum
+      puts cubes.values.map { |x| x[:exterior] }.sum
+    end
+
+    def self.walk(cubes, visited, x, y, z, depth)
+      depth += 1
+      return false unless visited["#{x}:#{y}:#{z}"].nil?
+      return true if depth == 2000
+      return true if x.negative? && y.negative? && z.negative?
+      return false unless cubes["#{x}:#{y}:#{z}"].nil?
+
+      visited["#{x}:#{y}:#{z}"] = true
+
+      adj = [
+        [x, y, z + 1],
+        [x, y, z - 1],
+        [x, y + 1, z],
+        [x, y - 1, z],
+        [x + 1, y, z],
+        [x - 1, y, z]
+      ]
+
+      adj.each do |a|
+        x, y, z = a
+        return true if walk(cubes, visited, x, y, z, depth)
+      end
+      false
     end
   end
 end
---
+++ solutions/19/second.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Day19
+  module Part2
+    def self.run(path, _) end
+  end
+end
