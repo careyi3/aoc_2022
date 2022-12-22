a6e8c2430e33539cf63e789e2dff381b3f8df742
careyi3 <ian@iancarey.ie>
2022-12-22 14:17:43 +0000

    Day 21 Part 1

--- solutions/21/first.rb
+++ solutions/21/first.rb
@@ -2,6 +2,19 @@
 
 module Day21
   module Part1
-    def self.run(path, _) end
+    def self.run(path, _)
+      monkeys = {}
+      FileReader.for_each_line(path) do |line|
+        key, val = line.split(': ')
+        monkeys[key] =
+          if val.to_i.zero?
+            x, op, y = val.split
+            -> { eval("monkeys['#{x}'].call #{op} monkeys['#{y}'].call") }
+          else
+            -> { val.to_i }
+          end
+      end
+      puts monkeys['root'].call
+    end
   end
 end
